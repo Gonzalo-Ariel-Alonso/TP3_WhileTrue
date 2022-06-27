60c2dd23f443ded01890210e263b0b8c7520573e
Eze Benza <ebenzaquen@fi.uba.ar>
2022-06-27 00:00:21 -0300

    compila pero hay que ver errores y que funcione el codigo nuevo

--- Descanso_entre_lecturas.h
+++ Descanso_entre_lecturas.h
@@ -16,44 +16,4 @@ public:
     int get_tiempo_de_descanso();
 };
 
-Descanso_entre_lecturas::Descanso_entre_lecturas(Tipo_de_lecturas lectura_actual,Tipo_de_lecturas lectura_siguiente)
-{
-    this->lectura_actual = lectura_actual;
-    this->lectura_siguiente = lectura_siguiente;
-}
-
-Descanso_entre_lecturas::~Descanso_entre_lecturas()
-{
-}
-
-void Descanso_entre_lecturas::set_tiempo_de_descanso(){
-    if ((lectura_actual == CUENTO && lectura_siguiente == NOVELA) || (lectura_actual == NOVELA && lectura_siguiente == CUENTO))
-        tiempo_de_descanso = 10;
-    else if ((lectura_actual == CUENTO && lectura_siguiente == NOVELA_HISTORICA) || (lectura_actual == NOVELA_HISTORICA && lectura_siguiente == CUENTO))
-        tiempo_de_descanso = 15;
-    else if ((lectura_actual == CUENTO && lectura_siguiente == POEMA) || (lectura_actual == POEMA && lectura_siguiente == CUENTO))
-        tiempo_de_descanso = 0;
-    else if ((lectura_actual == POEMA && lectura_siguiente == NOVELA) || (lectura_actual == NOVELA && lectura_siguiente == POEMA))
-        tiempo_de_descanso = 5;
-    else if ((lectura_actual == POEMA && lectura_siguiente == NOVELA_HISTORICA) || (lectura_actual == NOVELA_HISTORICA && lectura_siguiente == POEMA))
-        tiempo_de_descanso = 20;
-    else if ((lectura_actual == NOVELA && lectura_siguiente == NOVELA_HISTORICA) || (lectura_actual == NOVELA_HISTORICA && lectura_siguiente == NOVELA))
-        tiempo_de_descanso = 60;
-    else if (lectura_actual == POEMA && lectura_siguiente == POEMA)
-        tiempo_de_descanso = 1;
-    else if (lectura_actual == CUENTO && lectura_siguiente == CUENTO)
-        tiempo_de_descanso = 8;
-    else if (lectura_actual == NOVELA && lectura_siguiente == NOVELA)
-        tiempo_de_descanso = 30;
-    else if (lectura_actual == NOVELA_HISTORICA && lectura_siguiente == NOVELA_HISTORICA)
-        tiempo_de_descanso = 80;
-}
-
-int Descanso_entre_lecturas::get_tiempo_de_descanso(){
-    return tiempo_de_descanso;
-}
-
-
-
-
 #endif
