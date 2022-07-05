ce0024e1305376edbdb83375c0da38d42f2237f0
Gonzalo Alonso <gaalonso@fi.uba.ar>
2022-07-04 23:02:59 -0300

    comentarios agregados

--- Funciones.h
+++ Funciones.h
@@ -16,12 +16,26 @@ private:
 public:
     Funciones(Grafo<Lectura *> *grafo,Vector *vector_escritores);
     ~Funciones();
+    // PRE:
+    // POST: Imprime el grafo que tiene como atributo
     void imprimir_grafo();
+    // PRE:
+    // POST: Imprime la tabla de hash con sus respectivas colisiones en caso de existir
     void imprimir_tabla_hash();
+    // PRE:
+    // POST: Imprime arbol de expansion minima calculado con prim
     void imprimir_arbol_expansion_minima();
+    // PRE:
+    // POST: Elimina el primer vertice que se dio de alta en el Grafo
     void eliminar_primer_vertice();
+    // PRE:
+    // POST: Agrega un escritor con los parametro pasados por consola
     void agregar_escritor();
+    // PRE:
+    // POST: Muestra todos los escritores
     void catalogo_escritores();
+    // PRE:
+    // POST: Nos da a elegir un escritor para eliminarlo
     void eliminar_escritor();
 };
 
